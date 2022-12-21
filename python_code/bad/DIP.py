dbac6d755e5dcfdd26a0b919ef25f5d4f2cb115d
daehanh <daehan.han78@gmail.com>
2022-12-21 14:54:46 +0900

    ♻️ print update #1

--- python_code/bad/DIP.py
+++ python_code/bad/DIP.py
@@ -4,22 +4,27 @@
 class Worker(object):
 
     def work(self):
+
         print("I'm working!!")
 
 
 class Manager(object):
 
     def __init__(self):
+
         self.worker = None
 
     def set_worker(self, worker):
+
         assert isinstance(
             worker, Worker), '`worker` must be of type {}'.format(Worker)
 
         self.worker = worker
 
     def manage(self):
+
         if self.worker is not None:
+
             self.worker.work()
             # And some complex codes go here....
 
@@ -27,6 +32,7 @@ class Manager(object):
 class SuperWorker(object):
 
     def work(self):
+
         print("I work very hard!!!")
 
 # OK... now you can see what happend if we want the `Manager` to support `SuperWorker`.
@@ -38,14 +44,19 @@ class SuperWorker(object):
 def main():
 
     worker = Worker()
+
     manager = Manager()
+
     manager.set_worker(worker)
+
     manager.manage()
 
     # The following will not work...
     super_worker = SuperWorker()
+
     try:
         manager.set_worker(super_worker)
+        
     except AssertionError:
         print("manager fails to support super_worker....")
 
--- python_code/bad/LSP.py
+++ python_code/bad/LSP.py
@@ -8,36 +8,27 @@
 import copy
 
 class Person(object):
-
     def __init__(self, position):
         self.position = position
-
     def walk_north(self, dist):
         self.position[1] += dist
-
     def walk_east(self, dist):
         self.position[0] += dist
-
 # `Prisoner` is a logicall natural extension of `Person`
 # since they fulfill the "is-a" relation: a `Prisoner` is a `Person`.
 # However, such extension violate LSP in this case.
 class Prisoner(Person):
     PRISON_LOCATION = [3, 3]
-
     def __init__(self):
         super(Prisoner, self).__init__(copy.copy(self.PRISON_LOCATION))
         self.is_free = False
-
 # The issue here is that `Prisoner` inherite `walk_north` and `walk_east` methods
 # from the `Person` which is not logically correct for the `Prisoner` class.
-
 def main():
     prisoner = Prisoner()
-    print("The prisoner trying to walk to north by 10 and east by -3.")
-    
+    print("The prisoner trying to walk to north by 10 and east by -3.")    
     prisoner.walk_north(10)
-    prisoner.walk_east(-3)
-    
+    prisoner.walk_east(-3)    
     print("The location of the prison: {}".format(prisoner.PRISON_LOCATION))
     print("The current position of the prisoner: {}".format(prisoner.position))
 
