9813c8ecfe6df87b8a87315c35ca3d9fad701149
daehanh <daehan.han78@gmail.com>
2022-12-21 14:57:39 +0900

    ♻️ print update #1

--- python_code/bad/single_responsibility.py
+++ python_code/bad/single_responsibility.py
@@ -6,13 +6,15 @@ from abc import ABCMeta, abstractmethod
 class IEmail(object):
     __metaclass__ = ABCMeta
     @abstractmethod
-    def set_sender(self, sender):
+    def set_receiver(self,  receiver):
         pass
+    
     @abstractmethod
-    def set_receiver(self, receiver):
+    def set_sender(self,  sender):
         pass
+
     @abstractmethod
-    def set_content(self, content):
+    def set_content(self,  content):
         pass
 
 class Email(IEmail):
@@ -22,6 +24,12 @@ class Email(IEmail):
         self.__sender = None
         self.__receiver = None
         self.__content = None
+    def set_content(self, content):
+        if self.content_type == 'MyML':
+            self.__content = '\n'.join(['<myML>', content, '</myML>'])
+        else:
+            self.__content = content
+    
     def set_sender(self, sender):
         if self.protocol == 'IM':
             self.__sender = ''.join(["I'm ", sender])
@@ -32,21 +40,18 @@ class Email(IEmail):
             self.__receiver = ''.join(["I'm ", receiver])
         else:
             self.__receiver = receiver
-    def set_content(self, content):
-        if self.content_type == 'MyML':
-            self.__content = '\n'.join(['<myML>', content, '</myML>'])
-        else:
-            self.__content = content
+    
     def __repr__(self):
         template = "Sender: {sender}\nReceiver: {receiver}\nContent:\n{content}"
         return template.format(sender = self.__sender, receiver = self.__receiver, content = self.__content)
 def main():
     email = Email('IM', 'MyML')
-    email.set_sender('qmal')
     email.set_receiver('james')
+    email.set_sender('qmal')
     email.set_content('Hello, there!')
-    print(email)
     
+    print(email)
+
 
 if __name__ == '__main__':
     main()
